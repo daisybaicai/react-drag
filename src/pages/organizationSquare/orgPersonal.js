04284f35455bb1c40998971f6b1fe40a926d982a
daisybaicai <360619895@qq.com>
2020-04-18 23:22:35 +0800

    style: 整理创建组织所在的位置

--- src/pages/organizationSquare/orgPersonal.js
+++ src/pages/organizationSquare/orgPersonal.js
@@ -1,9 +1,15 @@
-import { List, Avatar, Icon } from 'antd';
+import { List, Button, Modal, Form, Input } from 'antd';
 import { connect } from 'dva';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const { TextArea } = Input;
 
 const OrginzationSquare = props => {
-  const { dispatch, orgList } = props;
+  const { dispatch, orgList, form } = props;
+  const { getFieldDecorator } = form;
+
+  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
     dispatch({
       type: 'orginzation/getPersonalOrganizationList',
@@ -21,22 +27,62 @@ const OrginzationSquare = props => {
     return null;
   };
 
+  /**
+   * @description 创建组织,打开弹窗
+   */
+  const createOrginzation = () => {
+    setVisible(true);
+  };
+
+  /**
+   * @description 关闭弹窗
+   */
+  const hideModal = () => {
+    setVisible(false);
+  };
+
+  /**
+   * @description 提交表单
+   * @param {*} e
+   */
+  const submitForm = e => {
+    const {
+      form: { validateFields },
+    } = props;
+    validateFields((err, value) => {
+      if (!err) {
+        let payload = {
+          orgName: value.orgName,
+          orgDescription: value.orgDescription,
+        };
+        dispatch({
+          type: 'orginzation/createOrginzation',
+          payload,
+        });
+        hideModal();
+      }
+    });
+  };
+
   return (
     <div>
       <div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button type="primary" onClick={createOrginzation}>
+            创建组织
+          </Button>
+        </div>
         <List
           itemLayout="vertical"
           size="large"
           pagination={{
-            pageSize: 3,
+            pageSize: 2,
           }}
           dataSource={orgList}
           renderItem={item => (
             <List.Item
               key={item.org_name}
-              actions={[
-                <RenderIcon item={item} />,
-              ]}
+              actions={[<RenderIcon item={item} />]}
               extra={
                 <img
                   width={272}
@@ -52,11 +98,35 @@ const OrginzationSquare = props => {
             </List.Item>
           )}
         />
+        <Modal
+          width="50%"
+          title="创建组织"
+          visible={visible}
+          onOk={submitForm}
+          onCancel={hideModal}
+          okText="确认"
+          cancelText="取消"
+        >
+          <div>
+            <Form labelCol={{ span: 4 }} wrapperCol={{ span: 14 }}>
+              <Form.Item label="组织名称">
+                {getFieldDecorator('orgName', {
+                  rules: [{ required: true, message: '请输入组件名称' }],
+                })(<Input />)}
+              </Form.Item>
+              <Form.Item label="组织描述">
+                {getFieldDecorator('orgDescription', {
+                  rules: [{ required: true, message: '请输入组织描述' }],
+                })(<TextArea />)}
+              </Form.Item>
+            </Form>
+          </div>
+        </Modal>
       </div>
     </div>
   );
 };
 
 export default connect(({ orginzation }) => ({ orgList: orginzation.mylist }))(
-  OrginzationSquare,
+  Form.create()(OrginzationSquare),
 );
