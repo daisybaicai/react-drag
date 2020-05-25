67d6ba37bf58bdfb25db776afa3d80ef030a3fcb
Dai Siyao <360619895@qq.com>
2020-05-25 18:37:22 +0800

    Revert "fix: 修复key的警告"
    This reverts commit edf4bb53578fa15ab609e68b631ea37517629fd1.

--- src/components/ActionMenu.js
+++ src/components/ActionMenu.js
@@ -90,7 +90,6 @@ const ActionMenu = props => {
             className={styles.btn}
             style={{ color: item.url === active ? '#1890FF' : '' }}
             onClick={() => handleClick(item)}
-            key={item.title}
           >
             <Icon type={item.icon} style={{ fontSize: '22px' }} />
             {item.title}
